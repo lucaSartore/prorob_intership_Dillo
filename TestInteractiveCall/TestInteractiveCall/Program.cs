3ad3f37e0b72de2425a5fd6c52a92033d0d6a95e
lucaSartore <lucasar.com8@gmail.com>
2023-07-27 16:31:17 +0200

    pre delete useless studd

--- TestInteractiveCall/TestInteractiveCall/Program.cs
+++ TestInteractiveCall/TestInteractiveCall/Program.cs
@@ -11,80 +11,107 @@ using Dillo.Voice.Extensions;
 using System.Text.Json;
 using System.Runtime.InteropServices;
 using Dillo.Voice.Models.Outbound;
+using Dillo.Voice.Builders;
+using Dillo.Voice.Constants.Say;
+using System.Threading;
+using Dillo.Voice.Constants.Transcribe;
+using Dillo.Voice.Models;
+using Dillo.Voice.Models.Actions;
+using static System.Net.Mime.MediaTypeNames;
+using System.Reflection.Emit;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+using Dillo.Voice.Constants.SendAction;
 
 const string API_KEY = "B14604DA5FA87D1BD5ABBF193F68948746673C16356328F7C7928E05A9AF2D4A";
 const string CUSTOMER_CODE = "783ec624-8136-4dc5-864a-0fbeb9b0a925";
 
 
+// the voice that will be used for the call
+SayVoice voice = new SayVoice
+{
+    LanguageCode = "it-IT",
+    VoiceId = "IsabellaNeural"
+};
+
+//setup the API key.
+DilloVoiceClient.Init(CUSTOMER_CODE, API_KEY);
+/*
+MenuBuilder menuBuilder = new MenuBuilder(timeout: 10)
+        .SetIntro(text: "Hello, for Sales, press 1. For Technical Support, press 2. To speak with an operator, press 3.", StandardVoices.EnglishUSMaleMatthew)
+        .AddDigit("1", ActionBuilder.Say(text: "You pressed 1 and chose Sales.", voice: StandardVoices.EnglishUSMaleMatthew))
+        .AddDigit("2", ActionBuilder.Say(text: "You pressed 2 and chose Technical Support.", voice: StandardVoices.EnglishUSMaleMatthew))
+        .RepeatOnNoDigit()
+        .RepeatOnWrongDigit();
+
+ChannelBehaviorBuilder channelBehaviorBuilder = new ChannelBehaviorBuilder()
+  .Menu(menuBuilder)
+  .Collect(finishOnKey: "#", timeout: 30, playBeep: true, numberOfDigits: 4);
+
+
+// decide sender and reciver
+OutboundVoiceRequestBuilder outboundRequestBuilder = new OutboundVoiceRequestBuilder(
+    sender: "+393391003110",
+    telephoneNumber: "+393703188051",
+    anonymizeInHours: 1.5)
+.SetChannelBehavior(channelBehaviorBuilder);
+
+// send the message
+OutboundVoiceRequest outboundVoiceRequest = outboundRequestBuilder.Build();*/
+
+string MessagePayload= File.ReadAllText("..\\..\\..\\Request_issue_call.json", Encoding.UTF8);
+
+//Console.WriteLine(MessagePayload);
+
 
 /*
-var options = new RestClientOptions("https://api.dillo.cloud")
+using (StreamWriter writer = new StreamWriter("C:\\Users\\luca.sartore\\Desktop\\SendAction.json"))
 {
-    MaxTimeout = -1,
-};
-
-var client = new RestClient(options);
-
-var request = new RestRequest("/api/sendaction", Method.Post);
-
-
-request.AddHeader("ApiKey", API_KEY);
-request.AddHeader("CustomerCode", CUSTOMER_CODE);
-
-request.AddHeader("Content-Type", "application/json");
-
-var body = @"{
-
-" + "\n" +
-
-@"    ""SendActionType"": ""Voice"",
-
-" + "\n" +
-
-@"    ""MessagePayload"": ""{\""ChannelBehavior\"":{\""ChannelBehaviorNodes\"":{\""0\"":{\""ActionType\"":\""Say\"",\""ChannelBehaviorAction\"":{\""Text\"":\""Testo Messaggio di alert. Il nodo 2 è di tipo play, l'ho inserito solo di esempio ma lo puoi saltare. Digita 1 per confermare l'intervento.\"",\""LanguageCode\"":\""it-IT\"",\""VoiceId\"":\""Bianca\""},\""NextNode\"":3},\""2\"":{\""ActionType\"":\""Play\"",\""ChannelBehaviorAction\"":{\""Url\"":\""https://ahdjkjjjjj.blob.core.windows.net/aaaaa-customeraudiofile/cpne_b6c11f57-aac3-49b5-9a09-d0a1c122f0e7.wav\""},\""NextNode\"":3},\""3\"":{\""ActionType\"":\""Menu\"",\""ChannelBehaviorAction\"":{\""Digits\"":{\""1\"":{\""NextNode\"":10}},\""Timeout\"":10,\""RecordMaxLength\"":5,\""PlayBeep\"":true,\""Recordings\"":[],\""StorageKey\"":\""ConfermaOrdine\"",\""VoicesExactMatch\"":false,\""OptionStatusCallbackUrl\"":\""https://realtimedigit.it\"",\""OptionStatusCallbackMethod\"":\""POST\"",\""RecordingStoragePeriodCode\"":\""3-M\"",\""Transcribe\"":false,\""LanguageCode\"":\""it-IT\""}},\""10\"":{\""ActionType\"":\""Say\"",\""ChannelBehaviorAction\"":{\""Text\"":\""Testo dopo la conferma\"",\""LanguageCode\"":\""it-IT\"",\""VoiceId\"":\""Bianca\""}}}},\""TelephoneNumber\"":\""+393703188051\"",\""StatusCallbackUrl\"":\""https://www.testcallback.it\"",\""Sender\"":\""+393391003110\""}""
-
-" + "\n" +
-
-@"}";
-request.AddStringBody(body, DataFormat.Json);
-
-RestResponse response = await client.ExecuteAsync(request);
-
-Console.WriteLine("Api response: " + response.Content);
-
-
-var responseContent_str = response.Content;*/
-
-var responseContent_str = "{ \"id\":\"c59be024-b63f-42d9-a08d-ee404de12a52\"}";
-
-ResponseContent? responseContent_row = JsonSerializer.Deserialize<ResponseContent>(responseContent_str);
-if (responseContent_row == null)
-{
-    throw new ArgumentNullException(nameof(responseContent_row));
+    writer.WriteLine(outboundVoiceRequest.SendActionType);
 }
-ResponseContent responseContent = responseContent_row;
+using (StreamWriter writer = new StreamWriter("C:\\Users\\luca.sartore\\Desktop\\MessagePayload.json"))
+{
+    writer.WriteLine(outboundVoiceRequest.MessagePayload);
+}
 
-Console.WriteLine("Api response: " + responseContent.id);
+throw new Exception();*/
 
 
-DilloVoiceClient.Init(CUSTOMER_CODE, API_KEY);
+OutboundVoicePostResponse postResponse = await outboundVoiceRequest.SendAsync();
 
+Console.WriteLine(JsonSerializer.Serialize<OutboundVoicePostResponse>(postResponse));
+
+
+// wait 45 seconds
+Thread.Sleep(35*1000);
+
+/*
 OutboundVoicePostResponse postResponse = new OutboundVoicePostResponse
 {
-    Id = new Guid("c59be024-b63f-42d9-a08d-ee404de12a52"),
+    Id = new Guid("788cbeb7-797b-4313-a073-e16424ef39d8"),
     IsSuccess = true,
-};
+};*/
 
-Console.WriteLine(postResponse.IsSuccess + " " + postResponse.Id.HasValue);
 
+
+
+
+// wait for the call to finish
 OutboundVoiceGetResponse getResponse = await postResponse.GetAsync();
+while (getResponse.Result.Status.PrimaryStatus.Code != 200){
+    getResponse = await postResponse.GetAsync();
+    Thread.Sleep(1000);
+}
 
-Console.WriteLine("Call response: " + getResponse.Result.TelephoneNumber);
+//Console.WriteLine("Call response: " + getResponse.Result.TelephoneNumber);
 
 Console.WriteLine(JsonSerializer.Serialize<OutboundVoiceRequestResult>(getResponse.Result));
 
-// struct to use for json desirialization
-public class ResponseContent
+
+string result = JsonSerializer.Serialize<OutboundVoiceRequestResult>(getResponse.Result);
+
+using (StreamWriter writer = new StreamWriter("C:\\Users\\luca.sartore\\Desktop\\file.json"))
 {
-    public string id { get; set; }
+    writer.WriteLine(result);
 }
